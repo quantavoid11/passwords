200f62164be75e02a1444a280e6ef351b3014d28
Siddhant Kumar Keshri <siddhantkeshri2806@gmail.com>
2023-10-28 18:55:20 +0530

    added copied! reaction

--- src/components/password/PasswordGenerator.jsx
+++ src/components/password/PasswordGenerator.jsx
@@ -1,12 +1,14 @@
 import { Copy } from 'lucide-react';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback,  useEffect, useRef, useState } from 'react';
 
 function PasswordGenerator() {
 
   const [length, setLength] = useState(6);
   const [numAllow, setNumAllow] = useState(false);
   const [charAllow, setCharAllow] = useState(false);
-  const [Password, setPassword] = useState("");
+  const [password, setPassword] = useState("");
+  const [isCopied,setIsCopied]=useState(false);
+
 
   const generatePassword = useCallback(() => {
 
@@ -28,25 +30,42 @@ function PasswordGenerator() {
 
   useEffect(()=>{
       generatePassword();
-  },[length,numAllow,charAllow,generatePassword])
+  },[length,numAllow,charAllow,generatePassword]);
 
+  const passwordRef=useRef(null);
+
+  const copyPassword=useCallback(()=>{
+
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(password);
+    setIsCopied(true);
+    setTimeout(()=>{setIsCopied(false)},1500);
+   
+
+  },[password,setIsCopied]
+  )
 
   return (
 
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
-      <div className="flex flex-col w-full bg-slate-900 rounded-xl px-2 py-2 max-w-md ">
+      <div className="flex flex-col  w-full bg-slate-900 rounded-xl px-2 py-2 max-w-md ">
         <h1 className="text-3xl text-white text-center mb-4 ">Password Generator</h1>
 
-        <div className="flex overflow-hidden rounded-lg shadow mb-4">
+        <div className="flex overflow-hidden rounded-lg shadow mb-4 mt-4 gap-x-2">
           <input type="text"
-            value={Password}
-            className="outline-none rounded-lg w-full py-1 px-3"
+            value={password}
+            className="outline-none rounded-lg w-full py-2 px-3"
             readOnly 
+            ref={passwordRef}
             />
-          <button className='shrink-0 px-3 text-white outline-none'><Copy /></button>
+          <button className='shrink-0 px-3 text-white outline-none ' onClick={copyPassword}>
+            <Copy />
+            </button>
+            {isCopied&&<span className='text-white py-2'>Copied!</span>}
+            
         </div>
 
-        <div className='flex gap-x-2 items-center'>
+        <div className='flex gap-x-2 items-center mt-4'>
 
           <div className='flex gap-x-1 items-center'>
             <input type="range" min={6} max={20} className='cursor-pointer' value={length}
@@ -67,7 +86,7 @@ function PasswordGenerator() {
 
         </div>
 
-        <div className=' flex justify-center text-centerh-10  '>
+        <div className=' flex justify-center text-centerh-10 mt-4 '>
           <button className='text-white cursor-pointer font-medium bg-blue-600  w-full rounded-md py-2 mt-4 mb-4 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500' onClick={generatePassword}>Regenerate</button>
           
 
